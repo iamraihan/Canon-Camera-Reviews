82037dad5fc3eca69e606111d5d519e1049308c2
iamraihan <trueraihan@gmail.com>
2022-04-04 21:01:58 +0600

    custom link added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 
 const Navbar = () => {
     return (
-        <nav className='flex gap-5 justify-center shadow-md h-14 items-center'>
-            <Link to='/'>Home</Link>
-            <Link to='/reviews'>Reviews</Link>
-            <Link to='/dashboard'>Dashboard</Link>
-            <Link to='/blogs'>Blogs</Link>
-            <Link to='/about'>About</Link>
+        <nav className='flex gap-5 justify-center  h-14 items-center uppercase'>
+            <CustomLink className='border-b-2 p-3' to='/'>Home</CustomLink>
+            <CustomLink className='border-b-2 p-3' to='/reviews'>Reviews</CustomLink>
+            <CustomLink className='border-b-2 p-3' to='/dashboard'>Dashboard</CustomLink>
+            <CustomLink className='border-b-2 p-3' to='/blogs'>Blogs</CustomLink>
+            <CustomLink className='border-b-2 p-3' to='/about'>About</CustomLink>
 
         </nav>
     );
