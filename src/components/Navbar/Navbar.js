f1d0b03083a81696ba06a5fdc0104589e1689cda
iamraihan <trueraihan@gmail.com>
2022-04-04 21:16:36 +0600

    navbar update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import CustomLink from '../CustomLink/CustomLink';
 
 const Navbar = () => {
     return (
-        <nav className='flex gap-5 justify-center  h-14 items-center uppercase'>
+        <nav className='md:flex gap-5 justify-center  h-14 items-center uppercase'>
             <CustomLink className='border-b-2 p-3' to='/'>Home</CustomLink>
             <CustomLink className='border-b-2 p-3' to='/reviews'>Reviews</CustomLink>
             <CustomLink className='border-b-2 p-3' to='/dashboard'>Dashboard</CustomLink>
