48ee1c241553c267aa6578676b79b669223f8ce4
iamraihan <trueraihan@gmail.com>
2022-04-05 12:50:28 +0600

    hamberger menu added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CustomLink from '../CustomLink/CustomLink';
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
     return (
-        <nav className='md:flex gap-5 justify-center  h-14 items-center uppercase'>
-            <CustomLink className='border-b-2 p-3' to='/'>Home</CustomLink>
-            <CustomLink className='border-b-2 p-3' to='/reviews'>Reviews</CustomLink>
-            <CustomLink className='border-b-2 p-3' to='/dashboard'>Dashboard</CustomLink>
-            <CustomLink className='border-b-2 p-3' to='/blogs'>Blogs</CustomLink>
-            <CustomLink className='border-b-2 p-3' to='/about'>About</CustomLink>
+        <div className='bg-white'>
+            <div onClick={() => setOpen(!open)} className='w-10 h-10 z-10 md:hidden'>
+                {open ? <XIcon className='h-8'></XIcon> : <MenuIcon></MenuIcon>}
+            </div>
+            <nav className={`md:flex  justify-center  md:h-14 items-center  uppercase absolute md:static bg-white z-0 w-full duration-500 ease-in ${open ? 'top-6 ' : 'top-[-120px]'}`}>
+                <CustomLink className='border-b-2 p-3 mr-8' to='/'>Home</CustomLink>
+                <CustomLink className='border-b-2 p-3 mr-8' to='/reviews'>Reviews</CustomLink>
+                <CustomLink className='border-b-2 p-3 mr-8' to='/dashboard'>Dashboard</CustomLink>
+                <CustomLink className='border-b-2 p-3 mr-8' to='/blogs'>Blogs</CustomLink>
+                <CustomLink className='border-b-2 p-3 mr-8' to='/about'>About</CustomLink>
 
-        </nav>
+            </nav>
+        </div>
     );
 };
 
